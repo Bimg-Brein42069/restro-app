97512129fa774b5ea0665d278a556fb23406e9de
Bimg-Brein42069 <dspani44100@gmail.com>
2024-05-30 20:44:11 +0530

    Added item filter for customer menu

--- frontend/src/pages/customerPages/CustomerWindow.tsx
+++ frontend/src/pages/customerPages/CustomerWindow.tsx
@@ -1,6 +1,6 @@
 import { IonButton, IonButtons, IonCard, IonCardContent, IonCol, IonContent, IonGrid, IonPage, IonRow, IonText, IonTitle } from '@ionic/react';
 import './CustomerWindow.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import React from 'react';
 import TopBarOrder from '../../components/topbars/TopBarOrder';
 import { useHistory, useParams } from 'react-router-dom';
@@ -18,7 +18,8 @@ const CustomerWindow: React.FC = () => {
   const [items,setItems] = useState<any []>([]);
   const history = useHistory();
   const tNo = useParams<{tableNo:string}>()
-
+  const [filter1,setFilter1] = useState<any []>([])
+  const [filter2,setFilter2] = useState<any []>([])
 
   useEffect(() => {
     fetchItems();
@@ -60,11 +61,47 @@ const CustomerWindow: React.FC = () => {
     location.reload()
   }
 
+  function filterfn(item:any){
+    const l1=filter1.length,l2=filter2.length;
+    if(l1 == 0 && l2 == 0)
+      return true;
+    if(l1 == 0)
+      return filter2.includes(item.type2)
+    if(l2 == 0)
+      return filter1.includes(item.type1)
+    return filter1.includes(item.type1) && filter2.includes(item.type2)
+  }
+  function changeFilter1(arg0: string) {
+    if(filter1.includes(arg0))
+      setFilter1(filter1 => filter1.filter((st)=>st!=arg0));
+    else
+      setFilter1(filter1 => [...filter1,arg0])
+  }
+
+  function changeFilter2(arg0: string) {
+    if(filter2.includes(arg0))
+      setFilter2(filter2 => filter2.filter((st)=>st!=arg0));
+    else
+      setFilter2(filter2 => [...filter2,arg0])
+  }
+
   function CustomerWindow2(){
+
     return (
       <div className="container">
+        <p><br></br></p> 
+        <IonButtons className='filter-buttons'>
+          <IonButton disabled={true}>Type of item:</IonButton>
+          {filter1.includes('Burger') ? <IonButton color='warning' onClick={() => changeFilter1('Burger')}>Burger</IonButton> : <IonButton onClick={() => changeFilter1('Burger')}>Burger</IonButton>}
+          {filter1.includes('Sandwich') ? <IonButton color='warning' onClick={() => changeFilter1('Sandwich')}>Sandwich</IonButton> : <IonButton onClick={() => changeFilter1('Sandwich')}>Sandwich</IonButton>}
+          {filter1.includes('Dessert') ? <IonButton color='warning' onClick={() => changeFilter1('Dessert')}>Dessert</IonButton> : <IonButton onClick={() => changeFilter1('Dessert')}>Dessert</IonButton>}
+        </IonButtons>
+        <IonButtons className='filter-buttons'>
+          {filter2.includes('Veg') ? <IonButton color='success' onClick={() => changeFilter2('Veg')}>Veg</IonButton> : <IonButton onClick={() => changeFilter2('Veg')}>Veg</IonButton>}
+          {filter2.includes('Non-Veg') ? <IonButton color='danger' onClick={() => changeFilter2('Non-Veg')}>Non-Veg</IonButton> : <IonButton onClick={() => changeFilter2('Non-Veg')}>Non-Veg</IonButton>}
+        </IonButtons>
         {
-          items.map(item => (
+          items.filter((item) => filterfn(item)).map(item => (
             <IonCard key={item.id}>
               <IonCardContent>
                 <strong>{item.name}</strong>
